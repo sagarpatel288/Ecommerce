92dff3663d90fcf34497788031c6387e404e61cb
srdpatel <sagarpatel288@gmail.com>
2020-02-19 02:04:29 +0530

    21.0 Get application context in ActivityMainViewModel through koin

--- app/src/main/java/com/example/android/ecommerce/viewmodels/ActivityMainViewModel.kt
+++ app/src/main/java/com/example/android/ecommerce/viewmodels/ActivityMainViewModel.kt
@@ -8,17 +8,18 @@ import com.example.android.ecommerce.model.Product
 import com.example.android.ecommerce.model.SortBy
 import com.example.android.ecommerce.repository.Repository
 import org.koin.core.KoinComponent
+import org.koin.core.get
 import org.koin.core.inject
 import java.util.*
 
 class ActivityMainViewModel : BaseViewModel(), KoinComponent {
     private val repository: Repository by inject()
-    val superParentCategoryList = MutableLiveData<List<Category>>()
-    val productList = MutableLiveData<List<Product>>()
-    val sortBy = MutableLiveData<SortBy>()
+    private val superParentCategoryList = MutableLiveData<List<Category>>()
+    private val productList = MutableLiveData<List<Product>>()
+    private val sortBy = MutableLiveData<SortBy>()
 
     fun getSortByOption(): MutableLiveData<SortBy> {
-        if (sortBy.value?.name.isNullOrEmpty()){
+        if (sortBy.value?.name.isNullOrEmpty()) {
             val sortByOption = SortBy("Category")
             sortBy.value = sortByOption
         }
@@ -26,7 +27,7 @@ class ActivityMainViewModel : BaseViewModel(), KoinComponent {
     }
 
     fun getParentCategories(context: Context): MutableLiveData<List<Category>> {
-        val superParents = repository.getParentCategories(context)
+        val superParents = repository.getParentCategories(get())
         superParentCategoryList.value = superParents
         return superParentCategoryList
     }
@@ -50,7 +51,7 @@ class ActivityMainViewModel : BaseViewModel(), KoinComponent {
     fun sortByViewed(sortBy: SortBy): MutableLiveData<List<Product>> {
         val products = repository.getProductListByViews()
         productList.value = products
-        if (!this.sortBy.value?.name.toString().toLowerCase().contains("viewed")){
+        if (!this.sortBy.value?.name.toString().toLowerCase().contains("viewed")) {
             this.sortBy.value = sortBy
         }
         return productList
@@ -59,7 +60,7 @@ class ActivityMainViewModel : BaseViewModel(), KoinComponent {
     fun sortByOrdered(sortBy: SortBy): MutableLiveData<List<Product>> {
         val products = repository.getProductListByOrder()
         productList.value = products
-        if (!this.sortBy.value?.name.toString().toLowerCase().contains("ordered")){
+        if (!this.sortBy.value?.name.toString().toLowerCase().contains("ordered")) {
             this.sortBy.value = sortBy
         }
         return productList
@@ -68,19 +69,16 @@ class ActivityMainViewModel : BaseViewModel(), KoinComponent {
     fun sortByShared(sortBy: SortBy): MutableLiveData<List<Product>> {
         val products = repository.getProductListBySharings()
         productList.value = products
-        if (!this.sortBy.value?.name.toString().toLowerCase().contains("shared")){
+        if (!this.sortBy.value?.name.toString().toLowerCase().contains("shared")) {
             this.sortBy.value = sortBy
         }
         return productList
     }
 
-    fun sortByCategory(
-        context: Context,
-        sortBy: SortBy
-    ): MutableLiveData<List<Category>> {
+    fun sortByCategory(context: Context, sortBy: SortBy): MutableLiveData<List<Category>> {
         val superParents = repository.getParentCategories(context)
         superParentCategoryList.value = superParents
-        if (!this.sortBy.value?.name.toString().toLowerCase().contains("category")){
+        if (!this.sortBy.value?.name.toString().toLowerCase().contains("category")) {
             this.sortBy.value = sortBy
         }
         return superParentCategoryList
